f840f24899c4ee57c57db3d32f27356b9503d8e3
Quickinline <adelbouhraoua23@gmail.com>
2021-01-05 15:17:21 +0100

    removed event-object

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const github = require('@actions/github');
 const input = {
     githubUsername: core.getInput('candidate-github-username'),
     testName: core.getInput('test-name'),
-    key: core.getInput('key'),
-    event: core.getInput('event-object')
+    key: core.getInput('key')
 }
 
 console.log(input)
